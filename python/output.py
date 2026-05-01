e7924758d8bd68e84f1a9a5e836e3a34ca0e1600
KFURU6 <90489907+KFURU6@users.noreply.github.com>
2026-05-01 22:34:35 +0900

    test5

--- python/output.py
+++ python/output.py
@@ -4,30 +4,30 @@ from collections import Counter
 text = "A=1000 B=500 C=1000 D=500 E=250"
 
 # 各鋼材のリスト
-data = [
-    # L形鋼
-    {"shape": "L", "thickness": 3, "width": 30, "depth": None},
-    {"shape": "L", "thickness": 3, "width": 40, "depth": None},
-    {"shape": "L", "thickness": 4, "width": 50, "depth": None},
-    {"shape": "L", "thickness": 6, "width": 50, "depth": None},
-    {"shape": "L", "thickness": 6, "width": 65, "depth": None},
-    {"shape": "L", "thickness": 6, "width": 75, "depth": None},
+# data = [
+#     # L形鋼
+#     {"shape": "L", "thickness": 3, "width": 30, "depth": None},
+#     {"shape": "L", "thickness": 3, "width": 40, "depth": None},
+#     {"shape": "L", "thickness": 4, "width": 50, "depth": None},
+#     {"shape": "L", "thickness": 6, "width": 50, "depth": None},
+#     {"shape": "L", "thickness": 6, "width": 65, "depth": None},
+#     {"shape": "L", "thickness": 6, "width": 75, "depth": None},
 
-    # 角パイプ（□）
-    {"shape": "□", "thickness": 1.6, "width": 25, "depth": None},
-    {"shape": "□", "thickness": 1.6, "width": 31, "depth": None},
-    {"shape": "□", "thickness": 1.6, "width": 40, "depth": None},
-    {"shape": "□", "thickness": 2.3, "width": 50, "depth": None},
-    {"shape": "□", "thickness": 2.3, "width": 45, "depth": 75},
-    {"shape": "□", "thickness": 2.3, "width": 50, "depth": 100},
-    {"shape": "□", "thickness": 3.2, "width": 75, "depth": 125},
+#     # 角パイプ（□）
+#     {"shape": "□", "thickness": 1.6, "width": 25, "depth": None},
+#     {"shape": "□", "thickness": 1.6, "width": 31, "depth": None},
+#     {"shape": "□", "thickness": 1.6, "width": 40, "depth": None},
+#     {"shape": "□", "thickness": 2.3, "width": 50, "depth": None},
+#     {"shape": "□", "thickness": 2.3, "width": 45, "depth": 75},
+#     {"shape": "□", "thickness": 2.3, "width": 50, "depth": 100},
+#     {"shape": "□", "thickness": 3.2, "width": 75, "depth": 125},
 
-    # チャンネル（C）
-    {"shape": "C", "thickness": 5, "width": 40, "depth": 75},
-    {"shape": "C", "thickness": 5, "width": 50, "depth": 100},
-    {"shape": "C", "thickness": 6, "width": 65, "depth": 125},
-    {"shape": "C", "thickness": 6.5, "width": 75, "depth": 150},
-    {"shape": "C", "thickness": 8, "width": 90, "depth": 200},
+#     # チャンネル（C）
+#     {"shape": "C", "thickness": 5, "width": 40, "depth": 75},
+#     {"shape": "C", "thickness": 5, "width": 50, "depth": 100},
+#     {"shape": "C", "thickness": 6, "width": 65, "depth": 125},
+#     {"shape": "C", "thickness": 6.5, "width": 75, "depth": 150},
+#     {"shape": "C", "thickness": 8, "width": 90, "depth": 200},
 
     # フラットバー（FB）
     {"shape": "FB", "thickness": 3, "width": 32, "depth": None},
