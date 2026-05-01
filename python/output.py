21fac9a38aacd03fad4da61a9e8f4be798d1e929
KFURU6 <90489907+KFURU6@users.noreply.github.com>
2026-05-01 21:42:28 +0900

    test2

--- python/output.py
+++ python/output.py
@@ -4,10 +4,38 @@ from collections import Counter
 text = "A=1000 B=500 C=1000 D=500 E=250"
 
 # 各鋼材のリスト
-# L-3×30,L-3×40,
-# □-5×50×50,
-# C-5×50×50,
-# FB-3×32,
+data = [
+    # L形鋼
+    {"shape": "L", "thickness": 3, "width": 30, "depth": None},
+    {"shape": "L", "thickness": 3, "width": 40, "depth": None},
+    {"shape": "L", "thickness": 4, "width": 50, "depth": None},
+    {"shape": "L", "thickness": 6, "width": 50, "depth": None},
+    {"shape": "L", "thickness": 6, "width": 65, "depth": None},
+    {"shape": "L", "thickness": 6, "width": 75, "depth": None},
+
+    # 角パイプ（□）
+    {"shape": "□", "thickness": 1.6, "width": 25, "depth": None},
+    {"shape": "□", "thickness": 1.6, "width": 31, "depth": None},
+    {"shape": "□", "thickness": 1.6, "width": 40, "depth": None},
+    {"shape": "□", "thickness": 2.3, "width": 50, "depth": None},
+    {"shape": "□", "thickness": 2.3, "width": 45, "depth": 75},
+    {"shape": "□", "thickness": 2.3, "width": 50, "depth": 100},
+    {"shape": "□", "thickness": 3.2, "width": 75, "depth": 125},
+
+    # チャンネル（C）
+    {"shape": "C", "thickness": 5, "width": 40, "depth": 75},
+    {"shape": "C", "thickness": 5, "width": 50, "depth": 100},
+    {"shape": "C", "thickness": 6, "width": 65, "depth": 125},
+    {"shape": "C", "thickness": 6.5, "width": 75, "depth": 150},
+    {"shape": "C", "thickness": 8, "width": 90, "depth": 200},
+
+    # フラットバー（FB）
+    {"shape": "FB", "thickness": 3, "width": 32, "depth": None},
+    {"shape": "FB", "thickness": 4.5, "width": 44, "depth": None},
+    {"shape": "FB", "thickness": 6, "width": 90, "depth": None},
+    {"shape": "FB", "thickness": 9, "width": 100, "depth": None},
+    {"shape": "FB", "thickness": 19, "width": 75, "depth": None},
+]
 
 # 数値抽出
 values = re.findall(r'\d+', text)
